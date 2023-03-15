ce621f2f5890e6dc643c3e50d03d44f3e32be9b4
katzm <mor.katz@autodesk.com>
2023-03-15 16:24:40 -0700

    Add flag for ascii encoding. Small refactor of dataloader.

--- models/byt5.py
+++ models/byt5.py
@@ -7,10 +7,10 @@ try:
     import comet_ml  # Import before torch
 except ImportError:
     pass
-import torch
 import torch.optim as optim
 import pytorch_lightning as pl
 from transformers import T5Config, T5ForConditionalGeneration, AutoTokenizer
+from util import get_quantized_range
 
 
 class ByT5Model(pl.LightningModule):
@@ -28,6 +28,18 @@ class ByT5Model(pl.LightningModule):
         self.model = model
         self.tokenizer = AutoTokenizer.from_pretrained(model_name)
         self.args = args
+
+        if args and args.ascii_encoding:
+            # Add new tokens to the tokenizer
+            quantize_n_bits = 6  # Hard code for now
+            self.tokenizer.add_tokens([f"<{i}>" for i in get_quantized_range(quantize_n_bits)])
+
+            # Add new token embeddings and initialize using learned embeddings
+            model.resize_token_embeddings(len(self.tokenizer))
+            embedding_params = model.get_input_embeddings().weight.data
+            for i in range(64):
+                # start with the embedding for 'A', ensures no clash with embedding for ';'
+                embedding_params[-i] = embedding_params[65 + i]
 
     def training_step(self, batch, batch_idx):
         outputs = self.model(**batch)
