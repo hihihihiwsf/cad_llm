cc5f85fbb4be29d680a1aff26cd591c17cc0131c
Amir KHAS AHMADI <amir.khasahmadi@autodesk.com>
2023-03-17 11:53:02 -0400

    polish

--- models/byt5.py
+++ models/byt5.py
@@ -95,6 +95,7 @@ class ByT5Model(pl.LightningModule):
         loss = outputs.loss
         self.log(f"val_loss", loss, on_step=False, on_epoch=True, prog_bar=True, logger=True)
         
+        
         if batch_idx % 10 == 0:
 
             df = self.calc_metric(batch)
