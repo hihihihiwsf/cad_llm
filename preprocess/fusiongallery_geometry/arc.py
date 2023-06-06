fed862915bf77f1286f7bea12571e6bdfc7aa784
Karl Willis <karl.willis@autodesk.com>
2023-06-06 16:28:12 -0700

    Finish arc implementation (untested)

--- preprocess/fusiongallery_geometry/arc.py
+++ preprocess/fusiongallery_geometry/arc.py
@@ -1,4 +1,5 @@
 import math
+import numpy as np
 
 from .base import FusionGalleryBase
 from .point import FusionGalleryPoint
@@ -52,15 +53,17 @@ class FusionGalleryArc(FusionGalleryBase):
             arc_dict["end_point"] = temp
         arc_dict["center_point"] = self.center.uuid
         arc_dict["radius"] = self.get_radius()
+
+        ref_vec = self.get_reference_vector()
         arc_dict["reference_vector"] = {
             "type": "Vector3D",
-            "x": ent.xDir,
-            "y": ent.yDir,
+            "x": ref_vec[0],
+            "y": ref_vec[1],
             "z": 0.0,
             "length": 1.0
         }
 
-        angle_x_to_dir = self.angle_from_vector_to_x([ent.xDir, ent.yDir])
+        angle_x_to_dir = FusionGalleryArc.angle_from_vector_to_x(ref_vec)
         center = self.merged_points[arc_dict["center_point"]]
         start_point = self.merged_points[arc_dict["start_point"]]
         end_point = self.merged_points[arc_dict["end_point"]]
@@ -74,3 +77,48 @@ class FusionGalleryArc(FusionGalleryBase):
     
     def get_radius(self):
         return math.sqrt((self.center.x - self.start.x) ** 2 + (self.center.y - self.start.y) ** 2)
+    
+    def get_reference_vector(self):
+        """"Get the unitized vector from the center point to the start point"""
+        return FusionGalleryArc.get_vector_between_pts(
+            self.center.point, self.start.point
+        )
+
+    @staticmethod
+    def get_vector_between_pts(pt1, pt2):
+        """Get the unit vector between two points"""
+        vec = pt2 - pt1
+        length = np.linalg.norm(vec)
+        if length > 1e-8:
+            nvec = vec / length
+        else:
+            nvec = [1, 0]
+        return nvec
+    
+    @staticmethod
+    def angle_from_vector_to_x(vec):
+        vec = [
+           np.clip(vec[0], -1, 1),
+           np.clip(vec[1], -1, 1)
+        ]
+        angle = 0.0
+        # 2 | 1
+        #-------
+        # 3 | 4
+        if vec[0] >=0:
+            if vec[1] >= 0:
+                # Qadrant 1
+                angle = math.asin(vec[1])
+            else:
+                # Qadrant 4
+                angle = 2.0*math.pi - math.asin(-vec[1])
+        else:
+            if vec[1] >= 0:
+                # Qadrant 2
+                angle = math.pi - math.asin(vec[1])
+            else:
+                # Qadrant 3
+                angle = math.pi + math.asin(-vec[1])
+        return angle
+
+
