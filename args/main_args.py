553789941c32e1394654ab1393a844bf12ebb2ac
katzm <mor.katz@autodesk.com>
2023-06-26 15:03:27 -0700

    Fix comment

--- args/main_args.py
+++ args/main_args.py
@@ -32,7 +32,7 @@ def get_parser():
     parser.add_argument("--seed", type=int, default=0, help="Random seed to use")
     parser.add_argument("--limit_data", type=float, default=1.0, help="Percentage of data to train on")
     # parser.add_argument("--val_every_n_epoch", type=int, default=1, help="Check validation after n training epochs")
-    parser.add_argument("--val_check_interval", type=float, default=1., help="Check validation after n training steps")
+    parser.add_argument("--val_check_interval", type=float, default=1., help="Check validation after fraction of epoch")
     parser.add_argument("--lora", type=int, default=0, help="Apply LoRA if true")
     parser.add_argument("--cosinedecay", type=int, default=1, help="Apply Cosine Learning rate decay if true")
 
