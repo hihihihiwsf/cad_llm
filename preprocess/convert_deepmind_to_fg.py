c218db765e72958c3b9d26912ecaec027d8399f2
Karl Willis <karl.willis@autodesk.com>
2023-07-07 12:18:07 -0700

    Save individual json files

--- preprocess/convert_deepmind_to_fg.py
+++ preprocess/convert_deepmind_to_fg.py
@@ -3,6 +3,12 @@
 Convert Deepmind sketch dataset sketches
 into the Fusion 360 Gallery dataset format
 
+The Deepmind sketch dataset comes in a protobuf format.
+Before using this script either convert from protobuf to json using:
+    preprocess/convertprotobuf.py
+or get the converted json files from someone who has already
+suffered through this process and taken one for the team.
+
 """
 
 import argparse
@@ -16,27 +22,34 @@ from preprocess.fusiongallery_geometry import *
 from preprocess.preprocess_utils import get_files, get_output_dir
 
 
-
-def convert_data(dm_data):
+def convert_data(dm_data, output_path, input_file, count):
+    """Convert all the sketches in a data file and save them as individual json files"""
     for index, dm_sketch in enumerate(tqdm(dm_data)):
-        fg_sketch = convert_sketch(dm_sketch, index)
+        fg_sketch = convert_sketch(dm_sketch, count)
         if fg_sketch is None:
-            print(f"Skipping sketch {index}")
+            print(f"Skipping sketch {count}")
         else:
-            # TODO: Do something with the FG sketch
-            pass
+            # Save the file with the name of the original deepmind data file
+            # and the index into that file of the sketch
+            json_file = output_path / f"{input_file.stem}_{index:06d}.json"
+            with open(json_file, "w") as f:
+                json.dump(fg_sketch, f, indent=4)
+        count += 1
+    return count
 
 
-def convert_sketch(dm_sketch, index):
+def convert_sketch(dm_sketch, count):
+    """Convert a single sketch in the deepmind sketch format into a Fusion Gallery sketch dict"""
     dm_entities = dm_sketch["entitySequence"]["entities"]
     dm_constraints = dm_sketch["constraintSequence"]["constraints"]
+    # First check that the sketch is good to convert
+    if not is_sketch_good(dm_entities):
+        return None
     points, point_map = create_sketch_points(dm_entities)
     curves, constraint_entity_map = create_sketch_curves(dm_entities, point_map)
-    if curves is None or len(curves) == 0:
-        return None
     constraints, dimensions = create_constraints_and_dimensions(dm_constraints, points, curves, constraint_entity_map)
     fusion_gallery_sketch = {
-        "name": f"Sketch{index}",
+        "name": f"Sketch{count}", # Save the sketch with the overall index in the dataset
         "type": "Sketch",
         "points": points,
         "curves": curves,
@@ -44,6 +57,43 @@ def convert_sketch(dm_sketch, index):
         "dimensions": dimensions
     }
     return fusion_gallery_sketch
+
+
+def is_sketch_good(dm_entities):
+    """Check that a sketch is valid and supported conversion"""
+    curve_count = 0
+    for dm_ent in dm_entities:
+        entity_type = list(dm_ent.keys())[0]
+        if entity_type == "pointEntity":
+            # Skip
+            pass
+        elif entity_type == "lineEntity":
+            curve_count += 1
+        elif entity_type == "circleArcEntity":
+            curve_count += 1
+        elif entity_type == "interpolatedSplineEntity":
+            print("Warning - interpolatedSplineEntity not supported")
+            return False
+        else:
+            print("Warning - unexpected entity type", entity_type)
+            return False
+    if curve_count == 0:
+        print("Warning - sketch doesn't have any curves")
+        return False
+    return True
+
+
+def create_sketch_points(dm_entities):
+    """Create the sketch points data structure"""
+    # point_map.map containts a dict of unique points
+    # key: string of the form point.x_point.y
+    # value: FusionGalleryPoint object
+    point_map = FusionGalleryPointMap(dm_entities)
+    # Sketch points data structure in the FG format
+    point_data = {}
+    for key, fg_point in point_map.map.items():
+        point_data[fg_point.uuid] = fg_point.to_dict()
+    return point_data, point_map  
 
 
 def create_sketch_curves(dm_entities, point_map):
@@ -56,10 +106,10 @@ def create_sketch_curves(dm_entities, point_map):
     constraint_entity_index = 0
     for dm_ent in dm_entities:
         assert len(dm_ent) == 1, "Expected on entry in the dict"
-        entity_name = list(dm_ent.keys())[0]
-        entity_data = dm_ent[entity_name]
+        entity_type = list(dm_ent.keys())[0]
+        entity_data = dm_ent[entity_type]
 
-        if entity_name == "pointEntity":
+        if entity_type == "pointEntity":
             dm_obj = DeepmindPoint(entity_data)
             # Find the point in the point map
             fg_obj = FusionGalleryPoint.from_xy_map(
@@ -68,12 +118,12 @@ def create_sketch_curves(dm_entities, point_map):
                 point_map.map
             )
 
-        elif entity_name == "lineEntity":
+        elif entity_type == "lineEntity":
             dm_obj = DeepmindLine(entity_data)
             fg_obj = FusionGalleryLine(dm_obj, point_map.map)
             add_curve = True
 
-        elif entity_name == "circleArcEntity":
+        elif entity_type == "circleArcEntity":
             if "arcParams" in entity_data:
                 dm_obj = DeepmindArc(entity_data)
                 fg_obj = FusionGalleryArc(dm_obj, point_map.map)
@@ -82,14 +132,6 @@ def create_sketch_curves(dm_entities, point_map):
                 dm_obj = DeepmindCircle(entity_data)
                 fg_obj = FusionGalleryCircle(dm_obj, point_map.map)
                 add_curve = True
-
-        elif entity_name == "interpolatedSplineEntity":
-            print("Warning - interpolatedSplineEntity not supported")
-            return None, None
-
-        else:
-            print("Warning - unexpected entity type", entity_name)
-            return None, None
 
         # Generate the FG dict that will get saved to json for the entity
         fg_dict = fg_obj.to_dict()
@@ -111,6 +153,7 @@ def create_sketch_curves(dm_entities, point_map):
 def update_constraint_entity_map(entity_map, fg_obj, fg_dict, index):
     """
     Update the given entity in the constraint entity map
+
     The key in the entity_map is an index counted based on a specific layout:
 
     entity0.part0, entity0.part1, ..., entity0.partN, entity1.part0, entity1.part1, ...
@@ -213,19 +256,6 @@ def update_constraint_entity_map(entity_map, fg_obj, fg_dict, index):
     return entity_map, updated_index
 
 
-def create_sketch_points(dm_entities):
-    """Create the sketch points data structure"""
-    # point_map.map containts a dict of unique points
-    # key: string of the form point.x_point.y
-    # value: FusionGalleryPoint object
-    point_map = FusionGalleryPointMap(dm_entities)
-    # Sketch points data structure in the FG format
-    point_data = {}
-    for key, fg_point in point_map.map.items():
-        point_data[fg_point.uuid] = fg_point.to_dict()
-    return point_data, point_map    
-
-
 def create_constraints_and_dimensions(dm_constraints, points, curves, constraint_entity_map):
     """Create the constraints and dimensions data structure"""
     constraints_data = {}
@@ -256,30 +286,29 @@ def create_constraint(constraint, points, curves, constraint_entity_map, constra
 
 def create_dimension(dimension, points, curves, constraint_entity_map, dimensions_data):
     """Create a constraint and add it to the provided constraints_data dictionary"""
-    dimension_dict = FusionGalleryDimension(dimension, points, curves, constraint_entity_map)
-    dimensions_data[dimension.uuid] = dimension_dict
+    dimension = FusionGalleryDimension(dimension, points, curves, constraint_entity_map)
+    dimensions_data[dimension.uuid] = dimension.to_dict()
 
 
 def main(args):
 
     input_path = Path(args.input)
     if input_path.is_dir():
-        input_file_paths = get_files(args.input, pattern="*.json")
-        input_file_paths.sort()
+        input_files = get_files(args.input, pattern="*.json")
+        input_files.sort()
     elif input_path.is_file():
-        input_file_paths = [input_path]
+        input_files = [input_path]
 
     output_path = get_output_dir(args.output)
 
-    for input_file_path in input_file_paths:
-        with open(input_file_path) as f:
+    # The data comes in 96 different data files
+    # that we loop over here
+    count = 0
+    for i, input_file in enumerate(input_files):
+        print(f"Converting {i}/{len(input_files)} data files")
+        with open(input_file) as f:
             dm_data = json.load(f)
-        convert_data(dm_data)
-
-        # filename = input_file_path.stem
-        # print(f"File {filename}: Converted {len(obj_data)} of {len(dm_data)} sketches to obj format")
-
-        # np.save(output_path / filename, obj_data, allow_pickle=True)
+        count += convert_data(dm_data, output_path, input_file, count)
 
 
 if __name__ == "__main__":
