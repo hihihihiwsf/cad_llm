178fcfa62ffe540265c29d2fcaf9d35442fd5e24
Karl Willis <karl.willis@autodesk.com>
2023-07-19 08:21:43 -0700

    Add sklearn dependency

---
+++ preprocess/convert_sketch_graphs_to_fg.py
@@ -0,0 +1,6 @@
+import preprocess.sketchgraphs.data as datalib
+from preprocess.sketchgraphs.data import flat_array, sequence
+
+from preprocess.sketchgraphs.data._entity import EntityType
+from preprocess.sketchgraphs.data._constraint import ConstraintType, ConstraintParameterType, DirectionValue
+from preprocess.sketchgraphs.pipeline import numerical_parameters
