1bc459525d310df62cfabea89e28dcfe22d71ba6
Amir KHAS AHMADI <amir.khasahmadi@autodesk.com>
2023-08-11 13:46:01 -0400

    END token is added

--- models/byt5.py
+++ models/byt5.py
@@ -625,9 +625,6 @@ class ByT5Model(pl.LightningModule):
         e_o = {}
         batch_2 = {}
         batch_2['encoder_hidden_states'] = unpacked_entities
-        #shifting to the right
-        # s_e = start_embed.unsqueeze(0).repeat(tgt.shape[0], 1, 1).to(self.device)
-        # batch_2['inputs_embeds'] = torch.concatenate((s_e, tgt[:, :-1, :]), dim=1)
         batch_2['input_ids'] = self.shift_right(o, self.model.config.decoder_start_token_id, self.model.config.pad_token_id)
         batch_2['attention_mask'] = batch['output_entities'].attention_mask[:, 1:]
         
@@ -639,14 +636,14 @@ class ByT5Model(pl.LightningModule):
         lbl[lbl == self.tokenizer.pad_token_id] = -100
         loss = torch.nn.functional.cross_entropy(lm_logits.permute(0, 2, 1), lbl, ignore_index=-100)
         
-        gt_generation = []
-        indices = np.append(0, np.cumsum(l))
-        max_logits = torch.argmax(lm_logits, dim=2)
-        for i in range(1, len(indices)):
-            gt_generation.append(max_logits[indices[i-1]:indices[i], :].reshape(1, -1).squeeze())
+        # gt_generation = []
+        # indices = np.append(0, np.cumsum(l))
+        # max_logits = torch.argmax(lm_logits, dim=2)
+        # for i in range(1, len(indices)):
+        #     gt_generation.append(max_logits[indices[i-1]:indices[i], :].reshape(1, -1).squeeze())
         
-        batch['samples'] = gt_generation
-        self.generate_samples(batch)
+        # batch['samples'] = gt_generation
+        # self.generate_samples(batch)
             
             
                     
@@ -731,6 +728,8 @@ class ByT5Model(pl.LightningModule):
         #                                  do_sample=False, max_new_tokens=self.args.max_length+10)
 
         string_samples = self.tokenizer.batch_decode(batch["samples"], skip_special_tokens=True)
+        for i, sample in enumerate(string_samples):
+            string_samples[i] = sample[:sample.find("END")]
         batch["point_samples"] = [get_point_entities(string_sample) for string_sample in string_samples]
         batch["point_labels"] = [get_point_entities(string_label) for string_label in batch["output_text"]]
 
