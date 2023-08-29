47298a7c10c8a4ca26a503c375b99478c9e03786
wus2 <wus2@git.autodesk.com>
2023-08-29 14:12:19 +0000

    compare subset without retrieval

--- main.py
+++ main.py
@@ -58,15 +58,15 @@ def main():
     
     print("Loading model...")
 
-    architecture = vl_byt5
+    architecture = byt5
     
     from transformers import ViTMAEForPreTraining 
     # vitmae_model = ViTMAEForPreTraining.from_pretrained("facebook/vit-mae-base")
     tokenizer = architecture.ByT5Model.get_tokenizer(args.model_name)
 
     print("Loading data...")
-    train_dataloader = get_icl_sketchgraphs_dataloader(tokenizer=tokenizer, args=args, split="train", shuffle=True)
-    val_dataloader = get_icl_sketchgraphs_dataloader(tokenizer=tokenizer, args=args, split="val", shuffle=False)
+    train_dataloader = get_sketchgraphs_dataloader(tokenizer=tokenizer, args=args, split="train", shuffle=True)
+    val_dataloader = get_sketchgraphs_dataloader(tokenizer=tokenizer, args=args, split="val", shuffle=False)
 
     num_train_batches = len(train_dataloader)
     num_gpus = torch.cuda.device_count()
