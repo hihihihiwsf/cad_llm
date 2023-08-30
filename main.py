3ff5864648c4a0e730aceec13cbb8208990dd6b9
wus2 <wus2@git.autodesk.com>
2023-08-30 02:17:17 +0000

    add test dataloader

--- main.py
+++ main.py
@@ -64,6 +64,7 @@ def main():
     print("Loading data...")
     train_dataloader = get_sketchgraphs_dataloader(tokenizer=tokenizer, args=args, split="train", shuffle=True)
     val_dataloader = get_sketchgraphs_dataloader(tokenizer=tokenizer, args=args, split="val", shuffle=False)
+    test_dataloader = get_sketchgraphs_dataloader(tokenizer=tokenizer, args=args, split="test", shuffle=False)
 
     call_backs = get_checkpoint_callbacks(log_dir=results_dir, all_checkpoint_dir=checkpoint_dir,
                                           using_sagemaker=args.using_sagemaker)
@@ -91,6 +92,7 @@ def main():
     if not args.eval: 
         print("Start training")
         trainer.fit(model, train_dataloaders=train_dataloader, val_dataloaders=val_dataloader)
+        trainer.test(model, dataloaders=test_dataloader, ckpt_path='best')
     else:
         # loading the model from exp_name/best.ckpt
         print("Start evaluating")
