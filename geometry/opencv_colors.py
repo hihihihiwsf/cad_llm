1ad3fb51a9390545e6f438eeef1226128edf0f82
Ubuntu <ubuntu@ip-10-241-203-119.us-west-2.compute.internal>
2023-09-12 01:57:43 +0000

    change clor to 1

--- geometry/opencv_colors.py
+++ geometry/opencv_colors.py
@@ -1,8 +1,15 @@
 # OpenCV is BGR (Pillow is RGB)
 
+# CV2_COLORS = {
+#     "blue": (255, 0, 0),
+#     "green": (0, 255, 0),
+#     "red": (0, 0, 255),
+#     "black": (0, 0, 0),
+# }
+
 CV2_COLORS = {
-    "blue": (255, 0, 0),
-    "green": (0, 255, 0),
-    "red": (0, 0, 255),
-    "black": (0, 0, 0),
+    "blue": 1,
+    "green":1,
+    "red": 1,
+    "black": 1,
 }
