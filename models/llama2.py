2d5969d968a6e63009b11313e2a7d8a441f4a78b
Amir KHAS AHMADI <amir.khasahmadi@autodesk.com>
2023-10-17 16:50:01 -0400

    halved the number of new tokens to generate

--- models/llama2.py
+++ models/llama2.py
@@ -125,7 +125,7 @@ class Llama2Model(pl.LightningModule):
         # Recursively unwrap the model from potential distributed training containers
         # generate_func = unwrap_model(self.model).generate
         pred_tokens = self.model.generate(input_ids=val_batch["generation_input_ids"], attention_mask=val_batch["generation_attention_mask"],
-                                    do_sample=False, max_new_tokens=self.max_length+10)
+                                    do_sample=False, max_new_tokens=int(self.max_length/2))
         batch_pred = self.tokenizer.batch_decode_to_entities(pred_tokens, skip_special_tokens=True)
         batch_true = val_batch["output_entities"]
 
