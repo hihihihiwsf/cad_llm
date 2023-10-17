de2e1e9c09a72db639cd4e475e5e87f1e8866034
Amir KHAS AHMADI <amir.khasahmadi@autodesk.com>
2023-10-17 13:17:31 -0400

    debugging

--- dataset/sketch_strings_collator.py
+++ dataset/sketch_strings_collator.py
@@ -61,7 +61,7 @@ class SketchStringsCollator:
         output_text = [example['output_text'] for example in examples]
         name = [example['name'] for example in examples]
 
-        if 'blahblah' in self.model_name.lower():
+        if 'llama' in self.model_name.lower():
             batch = self.llama_collate_fn(examples, self.tokenizer, self.max_length)
             batch["input_text"] = input_text
             batch["output_text"] = output_text
